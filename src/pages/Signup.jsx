d8d02307716fb1e858a622dcf1fb1417e3018cb4
AliAksoyy <alifeyza4017@gmail.com>
2023-02-12 16:00:18 +0300

    added

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -5,36 +5,75 @@ import logo from "../assets/ali.webp"
 import "./Signup.css"
 
 
-function validateImg() {}
+
 
 const Signup = () => {
 
   const [email,setEmail] =useState("")
   const [password,setPassword]=useState("")
   const [name,setName]=useState("")
+  const [image,setImage]=useState(null)
+  const [uploadingImg,setUploadingImg]=useState(false)
+  const [imagePreview,setImagePreview]=useState("")
 
-  function handleSubmit(e){
-    e.preventDefault()
-    console.log(name,email,password);
-    setEmail("")
-    setName("")
-    setPassword("")
+
+  
+  function validateImg(e) {
+    const file =e.target.files[0]
+    if(file.size >1048576){
+      return alert("Max file size is 1mb")
+    }else {
+      setImage(file)
+      setImagePreview(URL.createObjectURL(file))
+      console.log(URL.createObjectURL(file))
+    }
   }
 
+  async function uploadImage () {
+      const data = new FormData();
+      console.log(data);
+      data.append("file", image);
+      data.append("upload_preset","xfg7kqtc")
+      try {
+        setUploadingImg(true)
+        const res = await fetch("https://api.cloudinary.com/v1_1/ddhnnnyes/image/upload", {
+          method:"post",
+          body:data
+        });
+        console.log(res)
+        const urlData = await res.json()
+        console.log(urlData)
+        setUploadingImg(false)
+        return urlData.url
+      }catch(err) {
+        setUploadingImg(false)
+        console.log(err)
+      }
+  }
+
+
+
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    if(!image) return alert("Please upload your profile picture")
+    const url = await uploadImage(image);
+    console.log(url)
+  }
 
 
   return (
     <Container>
     <Row>
     <Col md={7} className="d-flex align-items-center justify-content-center flex-direction-column">
-    <Form onSubmit={(e)=>handleSubmit(e)} style={{width:"80%", maxWidth:500}}>
+    <Form onSubmit={handleSubmit} style={{width:"80%", maxWidth:500}}>
         <h1 className="text-center">Create Account</h1>
         <div className="signup-profile-pic__container">
-          <img src={logo} className="signup-profile-pic" alt="" />
+          <img src={imagePreview || logo} className="signup-profile-pic" alt="" />
           <label htmlFor="image-upload" className='image-upload-label'>
             <i className='fas fa-plus-circle add-picture-icon'></i>
           </label>
-          <input type="file" id="image-upload" hidden-accept="image/png, image/jpeg" onChange={validateImg} />
+          <input type="file" id="image-upload" hidden accept="image/png, image/jpeg" onChange={validateImg} />
         </div>
       <Form.Group className="mb-3" controlId="formBasicName">
         <Form.Label>Username</Form.Label>
