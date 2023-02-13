2b07d3cabd592a9ea2f8a01f99476d0e0f64415c
AliAksoyy <alifeyza4017@gmail.com>
2023-02-13 08:18:23 +0300

    added

---
+++ src/app/store.js
@@ -0,0 +1,8 @@
+import {configureStore} from "@reduxjs/toolkit"
+import userSlice from "../features/userSlice"
+import appApi from "../services/appApi"
+
+
+const store = configureStore({
+    
+})
---
+++ src/features/userSlice.js
@@ -0,0 +1,28 @@
+import {createSlice} from "@reduxjs/toolkit";
+import appApi from "../services/appApi"
+
+const initialState = null
+
+
+const userSlice = createSlice({
+    name:"appApi",
+    initialState,
+    reducers: {
+        addNotifications:(state,{payload})=> {},
+        resetNotifications:(state,{payload})=> {},
+    },
+    extraReducers: (builder)=> {
+        // save user after signup
+        builder.addMatcher(appApi.endpoints.signupUser.matchFulfilled, (state,{payload})=>payload);
+        // save user after login
+        builder.addMatcher(appApi.endpoints.loginUser.matchFulfilled, (state,{payload})=> payload);
+        // logout :destroy user sessions
+        builder.addMatcher(appApi.endpoints.logoutUser.matchFulfilled, ()=> null)
+    }
+
+})
+
+export const {addNotifications,resetNotifications} =userSlice.actions;
+export default userSlice.reducer
+
+
